321aa795f8c7cdfaea0cf84674717bf3e3328acd
KorDP <nukedmitry@gmail.com>
2023-01-29 15:11:41 +0300

    Задача 25 сделана

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int AtoBDegree(int ac, int bc)
+{
+    int result = 1;
+    for (int i = 0; i < bc; i++)
+    {
+        result = result * ac;
+    }
+    return result;
+}
+
+int aToBDegree = AtoBDegree(a, b);
+
+Console.WriteLine($"{a} в степени {b} равно {aToBDegree}.");
