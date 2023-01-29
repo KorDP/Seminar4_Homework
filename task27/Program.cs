b79844cd12b50bbb3500afaa71fe7f7340352dfa
KorDP <nukedmitry@gmail.com>
2023-01-29 15:15:58 +0300

    Задача 27 сделана

---
+++ task27/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int SumDigits(int num)
+{
+    int res = num % 10;
+    if (num > 0)
+    {
+        for (int i = 0; num / 10 > 0; i++)
+        {
+            num = num / 10;
+            res = res + num % 10;
+        }
+        return res;
+    }
+    else
+    {
+        Console.WriteLine("Введите положительное число!");
+        return 0;
+    }
+}
+
+int sumDigits = SumDigits(n);
+
+Console.WriteLine($"Сумма чисел числа {n} равна {sumDigits}.");
